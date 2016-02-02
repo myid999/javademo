9756148e7985805529fde272d941de2ee5404b16
zhou <zhou@zhou-PC>
2016-02-02 23:11:48 +0800

    0202h

---
+++ logdemo/jdklog/src/main/java/demo/java/log/JdkLoggingTest.java
@@ -0,0 +1,13 @@
+package demo.java.log;
+
+import java.util.logging.Logger;
+
+public class JdkLoggingTest {
+
+	private static final Logger logger=Logger.getLogger(JdkLoggingTest.class.getName());
+
+	public static void main(String[] args){
+	    logger.info("jdk logging info: a msg");
+	}
+
+}
