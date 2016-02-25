c583566d2e00011575ad8252791119bc2693e05b
zhou <zhou@zhou-PC>
2016-02-25 23:05:12 +0800

    0225h

---
+++ webservice/axis2demo/src/main/java/demo/axis2/service/CalculateService.java
@@ -0,0 +1,8 @@
+package demo.axis2.service;
+
+public interface CalculateService {  
+    public float plus(float x, float y);
+    public float minus(float x, float y);
+    public float multiply(float x, float y);
+    public float divide(float x, float y);
+}  
